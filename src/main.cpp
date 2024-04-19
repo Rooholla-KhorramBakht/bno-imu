b8a1ad795af1aade024b8845add28b3d38bce8ee
Rooholla-KhorramBakht <learn.kho@gmail.com>
2024-04-19 12:45:26 -0400

    sparkfun driver is added.

--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,134 @@
+#include <Arduino.h>
+#include <Wire.h>
 #include <SPI.h>
-#include <bno55.hpp>
-#include <bno08x.hpp>
-#include <shm.hpp>
-
-#include <utility/imumaths.h>
 
 
-#ifdef USE_BNO055
-  bno_imu::BNO055 imu_sensor = bno_imu::BNO055();
-#else
-  bno_imu::BNO08X imu_sensor = bno_imu::BNO08X();
-#endif
+#include <Wire.h>
 
-void setup(void)
+#include "SparkFun_BNO080_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_BNO080
+
+
+BNO080 myIMU;
+
+// indicators of new data availability
+volatile byte newQuat = 0;
+volatile byte newLinAcc = 0;
+// internal copies of the IMU data
+float ax, ay, az, qx, qy, qz, qw; // (qx, qy, qz, qw = i,j,k, real)
+byte linAccuracy = 0;
+float quatRadianAccuracy = 0;
+byte quatAccuracy = 0;
+// pin used for interrupts
+byte imuINTPin = 8;
+
+//Given an accuracy number, print what it means
+void printAccuracyLevel(byte accuracyNumber)
 {
-  packet.data.h1 = 'a';
-  packet.data.h2 = 'b';
-  packet.data.h3 = 'c';
-  packet.data.h4 = '\n';
+  if (accuracyNumber == 0) Serial.println(F("Unreliable"));
+  else if (accuracyNumber == 1) Serial.println(F("Low"));
+  else if (accuracyNumber == 2) Serial.println(F("Medium"));
+  else if (accuracyNumber == 3) Serial.println(F("High"));
+}
+// This function is called whenever an interrupt is detected by the arduino
+void interrupt_handler()
+{
+  // code snippet from ya-mouse
+   switch (myIMU.getReadings())
+   {
+      case SENSOR_REPORTID_LINEAR_ACCELERATION: {
+        newLinAcc = 1;
+      }
+      break;
+
+      case SENSOR_REPORTID_ROTATION_VECTOR:
+      case SENSOR_REPORTID_GAME_ROTATION_VECTOR: {
+         newQuat = 1;
+      }
+      break;
+      default:
+         // Unhandled Input Report
+         break;
+   }
+}
+
+void setup()
+{
   Serial.begin(115200);
+  Serial.println();
+  Serial.println("BNO080 Read example with Interrupt handler, getReading and multiple access");
 
-  while (!Serial) delay(10);  // wait for serial port to open!
-  imu_sensor.init();
+  Wire.begin();
 
-  delay(1000);
+  //Are you using a ESP? Check this issue for more information: https://github.com/sparkfun/SparkFun_BNO080_Arduino_Library/issues/16
+//  //=================================
+//  delay(100); //  Wait for BNO to boot
+//  // Start i2c and BNO080
+//  Wire.flush();   // Reset I2C
+//  IMU.begin(BNO080_DEFAULT_ADDRESS, Wire);
+//  Wire.begin(4, 5);
+//  Wire.setClockStretchLimit(4000);
+//  //=================================
+
+  //myIMU.enableDebugging(); // Uncomment this line to enable debug messages on Serial
+
+  // imuINTPin is used as an active-low interrupt. .begin configures the pinMode as INPUT_PULLUP
+  if (myIMU.begin(BNO080_DEFAULT_ADDRESS, Wire, imuINTPin) == false)
+  {
+    Serial.println("BNO080 not detected at default I2C address. Check your jumpers and the hookup guide. Freezing...");
+    while (1);
+  }
+
+  Wire.setClock(400000); //Increase I2C data rate to 400kHz
+
+  // prepare interrupt on falling edge (= signal of new data available)
+  attachInterrupt(digitalPinToInterrupt(imuINTPin), interrupt_handler, FALLING);
+  // enable interrupts right away to not miss first reports
+  interrupts();
+
+  // myIMU.enableLinearAccelerometer(50);  // m/s^2 no gravity, data update every 50 ms
+  myIMU.enableRotationVector(5); //Send data update every 100 ms
+
+  Serial.println(F("LinearAccelerometer enabled, Output in form x, y, z, accuracy, in m/s^2"));
+  Serial.println(F("Rotation vector, Output in form i, j, k, real, accuracy"));
+
 }
 
-void loop(void)
+
+
+void loop()
 {
-    imu_sensor.get_data(&packet);
-    Serial.write(packet.buffer, 4*16 + 4);
+  // Serial.println("Doing other things");
+  delay(10); //You can do many other things. Interrupt handler will retrieve latest data
 
-    // Serial.print(packet.data.q[0]); Serial.print(" "); Serial.print(packet.data.q[1]);  Serial.print(" ");
-    // Serial.print(packet.data.q[2]); Serial.print(" "); Serial.print(packet.data.q[3]);  Serial.println(" ");
-
-    delay(10);
+  // If new data on LinAcc sensor, retrieve it from the library memory
+  if(newLinAcc) {
+    // myIMU.getLinAccel(ax, ay, az, linAccuracy);
+    // newLinAcc = 0; // mark data as read
+    // Serial.print(F("acc :"));
+    // Serial.print(ax, 2);
+    // Serial.print(F(","));
+    // Serial.print(ay, 2);
+    // Serial.print(F(","));
+    // Serial.print(az, 2);
+    // Serial.print(F(","));
+    // Serial.print(az, 2);
+    // Serial.print(F(","));
+    // printAccuracyLevel(linAccuracy);
+  }
+  // If new data on Rotation Vector sensor, retrieve it from the library memory
+  if(newQuat) {
+    myIMU.getQuat(qx, qy, qz, qw, quatRadianAccuracy, quatAccuracy);
+    newQuat = 0; // mark data as read
+    Serial.print(F("quat:"));
+    Serial.print(qx, 2);
+    Serial.print(F(","));
+    Serial.print(qy, 2);
+    Serial.print(F(","));
+    Serial.print(qz, 2);
+    Serial.print(F(","));
+    Serial.print(qw, 2);
+    Serial.print(F(","));
+    printAccuracyLevel(quatAccuracy);
+  }
 }
-
-// void printEvent(sensors_event_t* event) {
-//   double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
-//   if (event->type == SENSOR_TYPE_ACCELEROMETER) {
-//     Serial.print("Accl:");
-//     x = event->acceleration.x;
-//     y = event->acceleration.y;
-//     z = event->acceleration.z;
-//   }
-//   else if (event->type == SENSOR_TYPE_ORIENTATION) {
-//     Serial.print("Orient:");
-//     x = event->orientation.x;
-//     y = event->orientation.y;
-//     z = event->orientation.z;
-//   }
-//   else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
-//     Serial.print("Mag:");
-//     x = event->magnetic.x;
-//     y = event->magnetic.y;
-//     z = event->magnetic.z;
-//   }
-//   else if (event->type == SENSOR_TYPE_GYROSCOPE) {
-//     Serial.print("Gyro:");
-//     x = event->gyro.x;
-//     y = event->gyro.y;
-//     z = event->gyro.z;
-//   }
-//   else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
-//     Serial.print("Rot:");
-//     x = event->gyro.x;
-//     y = event->gyro.y;
-//     z = event->gyro.z;
-//   }
-//   else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
-//     Serial.print("Linear:");
-//     x = event->acceleration.x;
-//     y = event->acceleration.y;
-//     z = event->acceleration.z;
-//   }
-//   else if (event->type == SENSOR_TYPE_GRAVITY) {
-//     Serial.print("Gravity:");
-//     x = event->acceleration.x;
-//     y = event->acceleration.y;
-//     z = event->acceleration.z;
-//   }
-//   else {
-//     Serial.print("Unk:");
-//   }
-
-//   Serial.print("\tx= ");
-//   Serial.print(x);
-//   Serial.print(" |\ty= ");
-//   Serial.print(y);
-//   Serial.print(" |\tz= ");
-//   Serial.println(z);
-// }
 
